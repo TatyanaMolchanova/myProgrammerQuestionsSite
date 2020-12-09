34b7cd37a1b1b73c7cac14ee6c79ff3d17c5db6f
Molchanova Tatyana <mtuspeh@gmail.com>
2020-12-09 08:34:03 +0200

    1 commit add header mat-toolbar

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDIFEY8HliuL6ShPvIBfGY2ZZPYagc14is',
+  fbDbUrl: 'https://blog-1659e.firebaseio.com'
+};
